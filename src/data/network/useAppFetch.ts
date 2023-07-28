f8d7b661ed5a697d6da96601a4de6d6fe1499156
Ilya Bakirov <everjupe13@gmail.com>
2023-07-29 02:26:13 +0300

    comment fetch hook

--- src/data/network/useAppFetch.ts
+++ src/data/network/useAppFetch.ts
@@ -1,32 +1,32 @@
-import { createFetch } from "@vueuse/core"
-import { isReactive, reactive } from "vue"
-import store from '@/store/index.js'
+// import { createFetch } from "@vueuse/core"
+// import { isReactive, reactive } from "vue"
+// import store from '@/store/index.js'
 
-export const API_URL = 'https://localhost:8080'
+// export const API_URL = 'https://localhost:8080'
 
-const useAppFetch = (...args) => {
-  const refStore = !isReactive(store) ? reactive(store) : store
+// const useAppFetch = (...args) => {
+//   const refStore = !isReactive(store) ? reactive(store) : store
 
-  const interceptors = {
-    async beforeFetch({ options }) {
-      const token = refStore.getters['auth/getToken']
-      if (token !== null) {
-        options.headers.Authorization = `Bearer ${token}`
-      }
+//   const interceptors = {
+//     async beforeFetch({ options }) {
+//       const token = refStore.getters['auth/getToken']
+//       if (token !== null) {
+//         options.headers.Authorization = `Bearer ${token}`
+//       }
 
-      return { options }
-    }
-  }
+//       return { options }
+//     }
+//   }
 
-  const useAppFetchFn = createFetch({
-    baseUrl: API_URL,
-    options: interceptors,
-    fetchOptions: {
-      mode: 'cors',
-    }
-  })
+//   const useAppFetchFn = createFetch({
+//     baseUrl: API_URL,
+//     options: interceptors,
+//     fetchOptions: {
+//       mode: 'cors',
+//     }
+//   })
 
-  return useAppFetchFn(...args)
-}
+//   return useAppFetchFn(...args)
+// }
 
-export default useAppFetch
+// export default useAppFetch
