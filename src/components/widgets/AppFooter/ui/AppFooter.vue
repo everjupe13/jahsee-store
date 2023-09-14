61cce4b2c583408872f1291edd4ffe927b519025
Ilya Bakirov <everjupe13@gmail.com>
2023-09-14 22:56:47 +0300

    feat: add animations

--- src/components/widgets/AppFooter/ui/AppFooter.vue
+++ src/components/widgets/AppFooter/ui/AppFooter.vue
@@ -13,65 +13,71 @@ import { SOCIAL_LINKS } from '../model'
 <template>
   <footer class="mt-auto flex items-center py-50">
     <AppContainer>
-      <div class="mb-10 flex items-center justify-center md:mb-10">
-        <p class="text">Contacts</p>
-      </div>
+      <div class="group mx-auto max-w-[500px]">
+        <div class="mb-10 flex items-center justify-center md:mb-10">
+          <p class="text">Contacts</p>
+        </div>
 
-      <div class="mb-20 flex items-center justify-center gap-x-20 md:mb-30">
-        <a
-          :href="SOCIAL_LINKS.TELEGRAM"
-          target="_blank"
-          rel="norefferer nofollow noopener"
-          class="px-3 py-5"
-        >
-          <SocialTelegramIcon
-            class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
-          />
-        </a>
-        <a
-          :href="SOCIAL_LINKS.INSTAGRAM"
-          target="_blank"
-          rel="norefferer nofollow noopener"
-          class="px-3 py-5"
-        >
-          <SocialInstagramIcon
-            class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
-          />
-        </a>
-        <a
-          :href="SOCIAL_LINKS.VK"
-          target="_blank"
-          rel="norefferer nofollow noopener"
-          class="px-3 py-5"
-        >
-          <SocialVkIcon
-            class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
-          />
-        </a>
-        <a
-          :href="SOCIAL_LINKS.X"
-          target="_blank"
-          rel="norefferer nofollow noopener"
-          class="px-3 py-5"
-        >
-          <SocialXIcon
-            class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
-          />
-        </a>
-        <a
-          :href="SOCIAL_LINKS.DISCORD"
-          target="_blank"
-          rel="norefferer nofollow noopener"
-          class="px-3 py-5"
-        >
-          <SocialDiscordIcon
-            class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
-          />
-        </a>
-      </div>
+        <div class="origin-top scale-75 transition-all group-hover:scale-100">
+          <div class="mb-20 flex items-center justify-center gap-x-20 md:mb-30">
+            <a
+              :href="SOCIAL_LINKS.TELEGRAM"
+              target="_blank"
+              rel="norefferer nofollow noopener"
+              class="px-3 py-5"
+            >
+              <SocialTelegramIcon
+                class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
+              />
+            </a>
+            <a
+              :href="SOCIAL_LINKS.INSTAGRAM"
+              target="_blank"
+              rel="norefferer nofollow noopener"
+              class="px-3 py-5"
+            >
+              <SocialInstagramIcon
+                class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
+              />
+            </a>
+            <a
+              :href="SOCIAL_LINKS.VK"
+              target="_blank"
+              rel="norefferer nofollow noopener"
+              class="px-3 py-5"
+            >
+              <SocialVkIcon
+                class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
+              />
+            </a>
+            <a
+              :href="SOCIAL_LINKS.X"
+              target="_blank"
+              rel="norefferer nofollow noopener"
+              class="px-3 py-5"
+            >
+              <SocialXIcon
+                class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
+              />
+            </a>
+            <a
+              :href="SOCIAL_LINKS.DISCORD"
+              target="_blank"
+              rel="norefferer nofollow noopener"
+              class="px-3 py-5"
+            >
+              <SocialDiscordIcon
+                class="h-30 w-30 fill-[#969eab] transition-colors hover:fill-white"
+              />
+            </a>
+          </div>
 
-      <div class="flex items-center justify-center">
-        <a href="/" class="text text-link">SUPPORT</a>
+          <div
+            class="flex translate-y-[-20px] items-center justify-center transition-all group-hover:translate-y-0"
+          >
+            <a href="/" class="text text-link">SUPPORT</a>
+          </div>
+        </div>
       </div>
     </AppContainer>
   </footer>
