a9c30f639db32f5cc95de185206f1069be09f9f7
Ilya Bakirov <everjupe13@gmail.com>
2023-09-16 00:55:13 +0300

    feat: update profile page / adaptive menu

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -10,6 +10,7 @@ import {
   AppProfileOrders
 } from '@/components/screens/app-profile'
 import { RouteNamesEnum } from '@/router/router.types'
+import { sleep } from '@/utils'
 
 const tabs = [
   {
@@ -17,7 +18,7 @@ const tabs = [
     name: 'profile',
     label: 'My profile',
     component: AppProfileInfo,
-    componentClasses: 'max-w-[640px]'
+    componentClasses: 'lg:max-w-[640px]'
   },
   {
     id: 1,
@@ -34,12 +35,15 @@ const tabs = [
 ]
 
 const activeTab = ref(0)
-const setActvieTab = (id: number) => {
+const setActiveTab = async (id: number) => {
   if (activeTab.value === id) {
+    openOnMobiles()
     return
   }
 
   activeTab.value = id
+  await sleep(200)
+  isMobileOpen.value = false
 }
 
 const router = useRouter()
@@ -50,32 +54,70 @@ const onLogoutClick = async () => {
   userStore.forget()
   return await router.push({ name: RouteNamesEnum.login })
 }
+
+const isMobileOpen = ref(false)
+const openOnMobiles = () => {
+  isMobileOpen.value = !isMobileOpen.value
+}
 </script>
 
 <template>
   <section class="pb-40 pt-30 md:pt-40">
     <AppContainer>
       <AppBackNav />
-      <div class="my-20 md:my-40">
-        <h1 class="outlined-text title font-hnd" style="--stroke-width: 2px">
+      <div class="my-20 xl:my-30 2xl:my-40">
+        <h1
+          class="outlined-text title relative font-hnd !text-[34px] font-bold uppercase leading-none sm:!text-[38px] lg:!text-[40px] xl:!text-[44px]"
+          style="--stroke-width: 2px"
+        >
           My account
         </h1>
       </div>
       <div class="cart-grid">
-        <div class="bg-gray px-50 py-40">
-          <nav class="flex flex-col gap-y-10">
+        <div
+          class="bg-gray px-20 py-15 lg:px-30 lg:py-20 xl:px-40 xl:py-30 2xl:px-50 2xl:py-40"
+        >
+          <nav class="flex flex-col">
             <div v-for="tab in tabs" :key="tab.id">
               <button
-                class="nav-link p-8 pl-0 font-hnd transition-colors"
-                :class="{ '--active': activeTab === tab.id }"
-                @click="setActvieTab(tab.id)"
+                class="nav-link mb-10 w-full p-8 pl-0 font-hnd transition-colors last:mb-0 lg:w-auto xl:p-8"
+                :class="{
+                  '--active': activeTab === tab.id,
+                  'max-xl:!block': isMobileOpen
+                }"
+                @click="setActiveTab(tab.id)"
               >
-                {{ tab.label }}
+                <span class="flex w-full items-center justify-between">
+                  <span class="flex flex-grow justify-start">
+                    {{ tab.label }}
+                  </span>
+                  <span
+                    v-if="activeTab === tab.id"
+                    class="flex flex-shrink-0 flex-grow-0 items-center justify-center lg:hidden"
+                  >
+                    <AppButton
+                      class="!px-14 !py-8 !text-[12px]"
+                      @click="openOnMobiles"
+                    >
+                      <span
+                        :class="[
+                          'relative h-10 w-10',
+                          'after:h-2 after:w-[10px] after:bg-white after:transition-all',
+                          'after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2',
+                          'before:h-10 before:w-2 before:bg-white before:transition-all',
+                          { 'before:h-2 before:w-[10px]': isMobileOpen },
+                          'before:absolute before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2'
+                        ]"
+                      ></span>
+                    </AppButton>
+                  </span>
+                </span>
               </button>
             </div>
             <div>
               <button
-                class="nav-link p-8 pl-0 font-hnd transition-colors"
+                class="nav-link p-4 pl-0 font-hnd transition-colors xl:p-8"
+                :class="{ '!inline-block': isMobileOpen }"
                 @click="onLogoutClick"
               >
                 Logout
@@ -84,7 +126,7 @@ const onLogoutClick = async () => {
           </nav>
         </div>
 
-        <div class="content-body min-h-[600px] bg-white p-40">
+        <div class="content-body min-h-[600px] bg-white px-20 py-30 lg:p-40">
           <component
             :is="tabs[activeTab].component"
             :class="tabs[activeTab].componentClasses"
@@ -99,15 +141,9 @@ const onLogoutClick = async () => {
 
 <style lang="scss" scoped>
 .title {
-  font-size: 64px;
-  font-style: normal;
-  font-weight: 800;
-  line-height: 100%;
-  letter-spacing: 2.56px;
-  text-transform: uppercase;
-
-  @media (max-width: 767px) {
-    font-size: 48px;
+  @include phone-big {
+    font-size: 36px;
+    -webkit-text-stroke-width: 1px;
   }
 }
 
@@ -148,7 +184,15 @@ const onLogoutClick = async () => {
   display: grid;
   grid-template-columns: 440px calc(100% - 440px);
 
-  @media (max-width: 991px) {
+  @include laptop-big {
+    grid-template-columns: 290px calc(100% - 300px);
+  }
+
+  @include laptop {
+    grid-template-columns: 260px calc(100% - 260px);
+  }
+
+  @include tablet {
     grid-template-columns: 100%;
   }
 }
@@ -162,6 +206,16 @@ const onLogoutClick = async () => {
   color: #848a99;
 
   @apply active:text-button-black/70;
+
+  @include laptop-big {
+    font-size: 20px;
+  }
+
+  @include tablet {
+    &:not(.--active) {
+      display: none;
+    }
+  }
 
   &.--active {
     color: #000;
