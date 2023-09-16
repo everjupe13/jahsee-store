8f728c1827e3190ef5421e884e87f6dfe81f8346
Ilya Bakirov <everjupe13@gmail.com>
2023-09-16 15:12:05 +0300

    feat: compose to config file

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { useCatalogStore } from '@/api/modules/catalog'
-import { HomeSlider } from '@/components/screens/app-home'
+import { HomeConfig } from '@/components/screens/app-home'
 
 const catalogStore = useCatalogStore()
 await catalogStore.fetchCatalog()
@@ -9,11 +9,7 @@ await catalogStore.fetchCatalog()
 <template>
   <section class="pb-40 pt-30 md:pt-40">
     <AppContainer>
-      <div
-        class="mx-auto w-full max-w-[500px] xl:max-w-[630px] 2xl:max-w-[800px]"
-      >
-        <HomeSlider />
-      </div>
+      <HomeConfig></HomeConfig>
     </AppContainer>
   </section>
 </template>
