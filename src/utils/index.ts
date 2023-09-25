1022494636ee3435801c85c34820ad7f3545eb6a
Ilya Bakirov <everjupe13@gmail.com>
2023-09-26 01:08:57 +0300

    fix: titles changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,12 @@ export const sleep = (ms: number) => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+const firstLetterCapitalize = (value?: string) =>
+  value && value.charAt(0) + value.slice(1)
+
 export const BRAND_NAME = 'AYAYSEE'
 export const constructPageTitle = (title?: string) => {
-  return [title?.toUpperCase(), BRAND_NAME].filter(Boolean).join(' - ')
+  const normalizedBrandName = firstLetterCapitalize(BRAND_NAME)
+  const normalizedTitle = firstLetterCapitalize(title)
+  return [normalizedTitle, normalizedBrandName].filter(Boolean).join(' - ')
 }
