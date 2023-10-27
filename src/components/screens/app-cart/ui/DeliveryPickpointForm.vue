8f7ebc534e95af6bdeaf1f71d9ff742cb378ba32
Ilya Bakirov <everjupe13@gmail.com>
2023-10-28 00:27:43 +0300

    feat: add searchbar

--- src/components/screens/app-cart/ui/DeliveryPickpointForm.vue
+++ src/components/screens/app-cart/ui/DeliveryPickpointForm.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import { computed, ref } from 'vue'
+
 type PickpointAddressType = {
   address: string
   code: string
@@ -31,42 +33,59 @@ const handleCurrentAddress = (code: string) => {
     emit('pick-address', code)
   }
 }
+
+const filterValue = ref('')
+const searchedAdresses = computed(() =>
+  filterValue.value
+    ? props.addresses.filter(address =>
+        address.address.toUpperCase().includes(filterValue.value.toUpperCase())
+      )
+    : props.addresses
+)
 </script>
 
 <template>
-  <form @submit.prevent="onSubmitForm">
+  <form
+    @submit.prevent="onSubmitForm"
+    class="h-full max-h-[80vh] min-h-[670px] flex-col"
+  >
     <h3
       class="mb-30 text-center font-hnd text-[30px] font-medium uppercase leading-tight tracking-[1.6px] text-button-black md:mb-40 md:text-[40px]"
     >
       delivery pickpoint
     </h3>
-    <template v-if="props.addresses.length > 0">
-      <div
-        v-for="address in props.addresses"
-        :key="address.code"
-        class="mb-20 text-[16px] font-semibold leading-none text-black"
-        @click="handleCurrentAddress(address.code)"
-      >
-        <div class="flex max-w-max cursor-pointer items-center gap-x-10">
-          <span
-            :class="[
-              'radio-icon',
-              { '--active': address.code === props.currentAddress?.code }
-            ]"
-          ></span>
-          <span>
-            {{ address.address }}
-          </span>
-        </div>
-      </div>
-    </template>
-    <template v-else>
-      <p
-        class="text-center text-[12px] font-semibold leading-none text-[#d84949] lg:text-[14px]"
-      >
-        {{ props.errorMessage || 'not found' }}
-      </p>
-    </template>
+    <AppInput v-model="filterValue" class="mb-20" placeholder="Search" />
+    <div class="max-h-[500px] flex-grow overflow-y-scroll py-5 transition-all">
+      <template v-if="props.addresses.length > 0">
+        <TransitionGroup name="list-complete">
+          <div
+            v-for="address in searchedAdresses"
+            :key="address.code"
+            class="mb-20 text-[16px] font-semibold leading-none text-black transition-all"
+            @click="handleCurrentAddress(address.code)"
+          >
+            <div class="flex max-w-max cursor-pointer items-center gap-x-10">
+              <span
+                :class="[
+                  'radio-icon',
+                  { '--active': address.code === props.currentAddress?.code }
+                ]"
+              ></span>
+              <span>
+                {{ address.address }}
+              </span>
+            </div>
+          </div>
+        </TransitionGroup>
+      </template>
+      <template v-else>
+        <p
+          class="text-center text-[12px] font-semibold leading-none text-[#d84949] lg:text-[14px]"
+        >
+          {{ props.errorMessage || 'not found' }}
+        </p>
+      </template>
+    </div>
   </form>
 </template>
 
@@ -118,5 +137,22 @@ const handleCurrentAddress = (code: string) => {
   &:checked::before {
     transform: scale(1);
   }
+}
+
+.list-complete-container,
+.list-complete-item {
+  transition: all 0.8s ease;
+}
+
+.list-complete-enter-from,
+.list-complete-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.list-complete-leave-active {
+  position: absolute;
+  transform: translateY(-10px);
+  width: 100%;
 }
 </style>
